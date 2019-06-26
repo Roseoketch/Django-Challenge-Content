a3d1b62eb8f8656e0b3aaff40fa4ee02f56c3eeb
rose.oketch <@oketchrose@gmail.com>
2019-06-26 11:35:58 +0300

    creating a fuction that converts and return day and its date

--- news/views.py
+++ news/views.py
@@ -13,8 +13,19 @@ def news_of_day(request):
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1>News for the {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
     return HttpResponse(html)
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
