d6beb2662ca85ffd80284f51a79aa5b5ccb12915
rose.oketch <@oketchrose@gmail.com>
2019-07-03 12:35:55 +0300

    changing urls and renaming the  views function

--- news/urls.py
+++ news/urls.py
@@ -5,7 +5,7 @@ from news import views
 
 urlpatterns=[
     # url(r'^$', views.welcome, name = 'welcome'),
-    url(r'^today/$', views.news_of_day, name = 'newsToday'),
+    url(r'^today/$', views.news_today, name = 'newsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name = 'pastNews') 
 
 
