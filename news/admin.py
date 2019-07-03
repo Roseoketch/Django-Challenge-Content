e1d4a4f87563b5e99150b97385d0e99305100f41
rose.oketch <@oketchrose@gmail.com>
2019-07-03 12:13:55 +0300

    registering models to admin file

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Editor,Article,tag
+
+admin.site.register(Editor)
+admin.site.register(Article)
+admin.site.register(tag)
