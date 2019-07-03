6fcb173bc7be094947aaec764721f0358f1a5452
rose.oketch <@oketchrose@gmail.com>
2019-07-03 12:23:15 +0300

    Adding phone field to database

--- news/admin.py
+++ news/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Editor,Article,tag
-
 admin.site.register(Editor)
 admin.site.register(Article)
 admin.site.register(tag)
