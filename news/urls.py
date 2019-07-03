6211ecbf43a407033db099ba003d7e7b9d311672
rose.oketch <@oketchrose@gmail.com>
2019-07-03 12:27:57 +0300

    deleting welcome urlconf on the appy

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,7 @@ from news import views
 # import datetime as dt
 
 urlpatterns=[
-    url(r'^$', views.welcome, name = 'welcome'),
+    # url(r'^$', views.welcome, name = 'welcome'),
     url(r'^today/$', views.news_of_day, name = 'newsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name = 'pastNews') 
 
