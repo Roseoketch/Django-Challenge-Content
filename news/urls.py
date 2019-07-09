6cf9d7d5ba180061dda37d1b0faf694f69fe8b42
rose.oketch <@oketchrose@gmail.com>
2019-07-09 11:54:52 +0300

    creating media urls

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from news import views
 from django.conf.urls.static import static
-# from django.conf import settings
+from django.conf import settings
 
 # import datetime as dt
 
@@ -13,5 +13,5 @@ urlpatterns=[
 
 ]
 
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
