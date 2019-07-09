a0ee83780d83b9f0f7ff717902bdfff2eb8057f1
rose.oketch <@oketchrose@gmail.com>
2019-07-09 11:06:52 +0300

    importing settings to urls in app

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from news import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 # import datetime as dt
 
@@ -10,3 +12,6 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results')
 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
